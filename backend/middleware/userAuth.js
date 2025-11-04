929ea866b9e8304e13d34b956ecedf5f454d8d16
Cristobal Ornelas <cdornelashernand6740@eagle.fgcu.edu>
2025-11-03 21:03:18 -0500

    create userRouters, userController, update userAuth, update server.js for app.use

--- backend/middleware/userAuth.js
+++ backend/middleware/userAuth.js
@@ -2,21 +2,17 @@ import jwt from "jsonwebtoken";
 
 const userAuth = async (req, res, next) => {
     const {token} = req.cookies;
-
     if (!token) {
         return res.json({success: false, message: "Not authorized. Login again"});
     }
 
     try{
 
-        const tokenDecoded = jwt.verify(token, process.env.JWT_SECRET);
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+        console.log("Good so far 1", tokenDecode?.id);
 
-        if (tokenDecoded.id){
-            req.body.userId = tokenDecoded.id
-        }
-        else{
-            return res.json({success: false, message: "Not authorized. Login Again"});
-        }
+        if (!req.body) req.body = {};        // <-- ensure body exists for GETs
+        req.body.userId = tokenDecode.id;    // attach for your controller
         next();
 
     }
