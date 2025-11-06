318dbb7d099bdc0c5bef88977278603da96ef57d
Cristobal Ornelas <cdornelashernand6740@eagle.fgcu.edu>
2025-11-05 20:05:10 -0500

    Create necessary jsx files for pages, update app.jsx, main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/home.jsx";
+import VerifyEmail from "./pages/verifyEmail.jsx";
+import Login from "./pages/login.jsx";
+import ResetPass from "./pages/resetPass.jsx";
 
 const App = () => {
     return (
         <div >
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/email-verify' element={<VerifyEmail/>}/>
+                <Route path='/reset-password' element={<ResetPass/>}/>
+            </Routes>
         </div>
     )
 }
